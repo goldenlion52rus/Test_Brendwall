2d40b9f305927135ba17ac63e9824eddeb0f04c6
Kirill Roimann <103315986+goldenlion52rus@users.noreply.github.com>
2024-09-12 10:48:39 +0300

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Product(models.Model):
+    """Модель для создания товара."""
+    
     title = models.CharField('Название', max_length=120)
     description = models.TextField('Описание')
     price = models.DecimalField(max_digits=10, decimal_places=2)
