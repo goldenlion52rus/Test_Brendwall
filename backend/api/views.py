06b76fac3adb657f4d890e2ec193325a91c40fb7
Kirill Roimann <103315986+goldenlion52rus@users.noreply.github.com>
2024-09-12 11:00:24 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,13 +6,18 @@ from .serializers import ProductSerializer
 
 
 class ProductView(ListCreateAPIView):
+    """Вьюха для получения списка всех товаров."""
+
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
 
 class CreateProductView(CreateAPIView):
+    """Вьюха для создания нового товара."""
+
     serializer_class = ProductSerializer
 
 
 def add_product(request):
+    """Вьюха для получения главной страницы приложения."""
     return render(request, 'index.html')
